9d12b66d52128b1ef1a37fbf4f3d27910aa47a0c
Wade Novak <wade.novak@gmail.com>
2015-03-24 16:33:27 -0500

    Begins styling sidebar and moves css to external file

--- content.js
+++ content.js
@@ -69,23 +69,13 @@ function toggleSidebar() {
     sidebar.id = "inventorySidebar";
     $(sidebar).html("\
       <h1>My Inventory</h1>\
+      <hr />\
       <div id='close-popup'>x</div>\
       <table>\
       <tbody id='inventory'>\
       </tbody>\
       </table>\
     ");
-    $(sidebar).css({
-      'position': 'fixed',
-      'top': '0px',
-      'right': '0px',
-      'width': '25%',
-      'height': '100%',
-      'background':'white',
-      'box-shadow': '0 0 5em black',
-      'z-index': '999999',
-      'display': 'none'
-    });
     $('body').append(sidebar);
     $(sidebar).toggle('slide', {'direction': 'right'}, 1000);
     $('body').animate({'width': '75%'}, 1000);
@@ -101,27 +91,53 @@ function toggleSidebar() {
     var streamlineAPI = 'https://machine-demo-app.herokuapp.com/machines.json';
     $.getJSON(streamlineAPI, function(data) {
       $(data).each(function(index, value) {
-        var stock_number = value['stock_number']
-        $('#inventory').append(
-          "<tr id='machine_" + stock_number + "'>" +
-          "<td id='stock_number'>" + stock_number + "</td>" +
-          "<td id='" + stock_number + "_make'>" + value['machine_make'] + "</td>" +
-          "<td id='" + stock_number + "_model'>" + value['machine_model'] + "</td>" +
-          "<td id='" + stock_number + "_type'>" + value['machine_type'] + "</td>" +
-          "<td>" + "<button class='copy_machine' id=" + value['stock_number'] + ">Copy</button></li>" + "</td>" +
-          "<td id='" + stock_number + "_year' hidden>" + value['year'] + "</td>" +
-          "<td id='" + stock_number + "_hours' hidden>" + value['hours'] + "</td>" +
-          "<td id='" + stock_number + "_price' hidden>" + value['price'] + "</td>" +
-          "<td id='" + stock_number + "_location' hidden>" + value['location'] + "</td>" +
-          "<td id='" + stock_number + "_serial' hidden>" + value['serial'] + "</td>" +
-          "<td id='" + stock_number + "_description' hidden>" + value['description'] + "</td>" +
-          "</tr>"
-        );
+        var locationAPI = 'https://machine-demo-app.herokuapp.com/locations/' + value['location'] + '.json';
+        $.getJSON(locationAPI, function(json) {
+          var JSONaddress = json.address;
+          var JSONcity = json.city;
+          var JSONstate = json.state;
+          var JSONzip = json.zip;
+          var JSONphone = json.phone;
+          var stock_number = value['stock_number']
+          $('#inventory').append(
+            "<tr class='machine-preview-row'>" +
+              "<td class='machine-preview-cell' colspan='3'>" +
+                "<strong>Stock " + stock_number + "</strong>" + " " +
+                "<small>" +
+                  value['machine_make'] + " " + value['machine_model'] + " " + value['machine_type'] +
+                "</small>" +
+              "</td>" +
+            "</tr>" +
+            "<tr class='machine-preview-description' id='machine_" + stock_number + "'>" +
+              "<td id='" + stock_number + "_hours'>" +
+                "<small>" + "Hours: " + value['hours'] + "</small>" +
+              "</td>" +
+              "<td id='" + stock_number + "_price'>" +
+                "<small>" + "Price: " + value['price'] + "</small>" +
+              "</td>" +
+              "<td id='" + stock_number + "_city'>" +
+                "<small>" + "Loc: " + JSONcity + "</small>" +
+              "</td>" +
+              // hidden values
+              "<td id='stock_number' hidden>" + stock_number + "</td>" +
+              "<td id='" + stock_number + "_make' hidden>" + value['machine_make'] + "</td>" +
+              "<td id='" + stock_number + "_model' hidden>" + value['machine_model'] + "</td>" +
+              "<td id='" + stock_number + "_type' hidden>" + value['machine_type'] + "</td>" +
+              "<td id='" + stock_number + "_year' hidden>" + value['year'] + "</td>" +
+              "<td id='" + stock_number + "_serial' hidden>" + value['serial'] + "</td>" +
+              "<td id='" + stock_number + "_address' hidden>" + JSONaddress + "</td>" +
+              "<td id='" + stock_number + "_state' hidden>" + JSONstate + "</td>" +
+              "<td id='" + stock_number + "_zip' hidden>" + JSONzip + "</td>" +
+              "<td id='" + stock_number + "_phone' hidden>" + JSONphone + "</td>" +
+              "<td id='" + stock_number + "_description' hidden>" + value['description'] + "</td>" +
+            "</tr>"
+          );
+        });
       });
     });
     sidebarOpen = true;
     sidebarLoaded = true;
-    // need to get content source attribute working to refresh iframes in MT
+    // not working: need to get content source attribute working to refresh iframes in MT
     $('#contentIFrame').attr('contentsrc', function (i, val) {
       return val;
     });
@@ -134,16 +150,20 @@ $(document).on('click', '#close-popup', function() {
   displayButton();
 });
 
-$(document).on('click', '.copy_machine', function(btn) {
+$(document).on('click', '.machine-preview-cell', function(btn) {
   var stock_number = btn.target.id;
-  var year         = $('#' + stock_number + '_year').html();
-  var make         = $('#' + stock_number + '_make').html();
-  var model        = $('#' + stock_number + '_model').html();
-  var type         = $('#' + stock_number + '_type').html();
-  var serial       = $('#' + stock_number + '_serial').html();
-  var hours        = $('#' + stock_number + '_hours').html();
-  var price        = $('#' + stock_number + '_price').html();
-  var location     = $('#' + stock_number + '_location').html();
+  var year            = $('#' + stock_number + '_year').html();
+  var make            = $('#' + stock_number + '_make').html();
+  var model           = $('#' + stock_number + '_model').html();
+  var type            = $('#' + stock_number + '_type').html();
+  var serial          = $('#' + stock_number + '_serial').html();
+  var hours           = $('#' + stock_number + '_hours').html();
+  var price           = $('#' + stock_number + '_price').html();
+  var address         = $('#' + stock_number + '_address').html();
+  var city            = $('#' + stock_number + '_city').html();
+  var state           = $('#' + stock_number + '_state').html();
+  var zip             = $('#' + stock_number + '_zip').html();
+  var phone           = $('#' + stock_number + '_phone').html();
   var description     = $('#' + stock_number + '_description').html();
 
   var elsREGEX = (/\w+:\/\/\w+.equipmentlocator.\w+\/*\w*/i);
@@ -161,13 +181,45 @@ $(document).on('click', '.copy_machine', function(btn) {
            );
     }
     else if ($('#EQUIPMENT_stock').val() === stock_number) {
-      $('#EQUIPMENT_year').val(year).effect('highlight', 'slow');
-      $('#EQUIPMENT_make').val(make).effect('highlight', 'slow');
-      $('#EQUIPMENT_model').val(model).effect('highlight', 'slow');
-      $('#EQUIPMENT_serial').val(serial).effect('highlight', 'slow');
-      $('#EQUIPMENT_hrs').val(hours).effect('highlight', 'slow');
-      $('#EQUIPMENT_price').val(price).effect('highlight', 'slow');
-      $('#EQUIPMENT_ecity').val(location).effect('highlight', 'slow');
+      if ($('#EQUIPMENT_control').val() !== phone) {
+        $('#EQUIPMENT_control').val(phone).effect('highlight', 'slow');
+      }
+
+      if ($('#EQUIPMENT_year').val() !== year) {
+        $('#EQUIPMENT_year').val(year).effect('highlight', 'slow');
+      }
+
+      if ($('#EQUIPMENT_make').val() !== make) {
+        $('#EQUIPMENT_make').val(make).effect('highlight', 'slow');
+      }
+
+      if ($('#EQUIPMENT_model').val() !== model) {
+        $('#EQUIPMENT_model').val(model).effect('highlight', 'slow');
+      }
+
+      if ($('#EQUIPMENT_serial').val() !== serial) {
+        $('#EQUIPMENT_serial').val(serial).effect('highlight', 'slow');
+      }
+
+      if ($('#EQUIPMENT_hrs').val() !== hours) {
+        $('#EQUIPMENT_hrs').val(hours).effect('highlight', 'slow');
+      }
+
+      if ($('#EQUIPMENT_price').val() !== price) {
+        $('#EQUIPMENT_price').val(price).effect('highlight', 'slow');
+      }
+
+      if ($('#EQUIPMENT_ecity').val() !== city) {
+        $('#EQUIPMENT_ecity').val(city).effect('highlight', 'slow');
+      }
+
+      if ($('#EQUIPMENT_estate').val() !== state) {
+        $('#EQUIPMENT_estate').val(state).effect('highlight', 'slow');
+      }
+
+      if ($('#EQUIP_NOTE_note').val() !== description) {
+        $('#EQUIP_NOTE_note').val(description).effect('highlight', 'slow');
+      }
     }
   }
 
@@ -203,6 +255,22 @@ $(document).on('click', '.copy_machine', function(btn) {
 
       if ($('#HOURS').val() !== hours) {
         $('#HOURS').val(hours).effect('highlight', 'slow');
+      }
+
+      if ($('#_PUBL_DLR_ADDRESS').val() !== address) {
+        $('#_PUBL_DLR_ADDRESS').val(address).effect('highlight', 'slow');
+      }
+
+      if ($('#_PUBL_DLR_CITY').val() !== city) {
+        $('#_PUBL_DLR_CITY').val(city).effect('highlight', 'slow');
+      }
+
+      if ($('#_STATE').val() !== state) {
+        $('#_STATE').val(state).effect('highlight', 'slow');
+      }
+
+      if ($('#_PUBL_DLR_ZIP').val() !== zip) {
+        $('#_PUBL_DLR_ZIP').val(zip).effect('highlight', 'slow');
       }
 
       if ($('#_RETAIL_PRICE').val() !== price) {
