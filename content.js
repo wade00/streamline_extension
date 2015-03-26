7c71f0f0abbc817e5f3915d6825682bd273cc13b
Wade Novak <wade.novak@gmail.com>
2015-03-26 16:54:38 -0500

    Creates separate load and display functions for button and sidebar

--- content.js
+++ content.js
@@ -12,10 +12,19 @@ function handleMessage(msg, sender, sendResponse) {
 }
 chrome.runtime.onMessage.addListener(handleMessage);
 
-// Inject show inventory button into page
-window.onload = displayButton();
+// Sets regexs for sites and stores tab url in variable
+var elsREGEX = (/\w+:\/\/\w+.equipmentlocator.\w+\/*\w*/i);
+var eaREGEX  = (/\w+:\/\/\w+.equipmentalley.\w+\/*\w*/i);
+var mtREGEX  = (/\w+:\/\/dscrm.sandhills.\w+\/*\S*/i);
+var siteURL  = document.URL;
+
+// Load show inventory button and sidebar into page DOM
+window.onload = loadButton();
+window.onload = loadSidebar();
+
+// Load button function
 var buttonDisplayed;
-function displayButton() {
+function loadButton() {
   var inventoryButton = document.createElement('div');
   inventoryButton.id = "showInventoryButton";
   $(inventoryButton).html("<p class='copy' hidden>Copy it.</p>")
@@ -23,8 +32,8 @@ function displayButton() {
     'position': 'fixed',
     'top': '30px',
     'right': '0px',
-    'width': '5%',
-    'height': '5%',
+    'width': '100px',
+    'height': '50px',
     'background': '#4A5063',
     'color': '#EEE',
     'text-align': 'center',
@@ -35,25 +44,100 @@ function displayButton() {
     'cursor': 'pointer',
     'display': 'none'
   });
+
+  // THIS IS WHAT I NEED TO MOVE INTO IF STATEMENT
   document.body.appendChild(inventoryButton);
   $(inventoryButton).effect('slide', {'direction': 'right'}, function() {
     $('.copy').toggle('fade', 'slow');
   });
-  buttonDisplayed = true;
+
+  buttonDisplayed = false;
 }
 
+// Hover states for button
 $('#showInventoryButton').hover(function(){
-    $(this).animate({'width': '6%',
+    $(this).animate({'width': '125px',
                      'background-color': '#3B404F',
                      'color': '#FFF',
                      }, 500);
   }, function() {
-    $(this).animate({'width': '5%',
+    $(this).animate({'width': '100px',
                      'background-color': '#4A5063',
                      'color': '#EEE',
                      }, 500);
 });
 
+// Load sidebar function
+var sidebarOpen;
+function loadSidebar() {
+  var sidebar = document.createElement('div');
+  sidebar.id = "inventorySidebar";
+  $(sidebar).html("\
+    <div class='header'>\
+      <h2 class='streamline-logo'>Streamline <span id='close-popup'>x</span></h2>\
+    </div>\
+    <hr class='divider' />\
+    <h3 class='my-inventory-header'>My Inventory</h3>\
+    <div class='inventory-table-container'>\
+      <table class='inventory-table'>\
+        <tbody id='inventory'>\
+        </tbody>\
+      </table>\
+    </div>\
+  ");
+  $('body').append(sidebar);
+
+  // Loads inventory into sidebar by parsing json from heroku site
+  var streamlineAPI = 'https://machine-demo-app.herokuapp.com/machines.json';
+  $.getJSON(streamlineAPI, function(data) {
+    $(data).each(function(index, value) {
+      var locationAPI = 'https://machine-demo-app.herokuapp.com/locations/' + value['location'] + '.json';
+      $.getJSON(locationAPI, function(json) {
+        var JSONaddress = json.address;
+        var JSONcity = json.city;
+        var JSONstate = json.state;
+        var JSONzip = json.zip;
+        var JSONphone = json.phone;
+        var JSONeaAccount = json.equipment_alley_account;
+        var stock_number = value['stock_number'];
+        $('#inventory').append(
+          "<tr class='machine-preview'>" +
+            "<td class='machine-preview-cell' colspan='3' id='" + stock_number + "'>" +
+              "<strong class='stock-number' id='" + stock_number + "'>" + stock_number + "</strong>" + " " +
+              "<small id='" + stock_number + "'>" +
+                value['machine_make'] + " " + value['machine_model'] + " " + value['machine_type'] +
+              "</small>" + " " +
+            "</td>" +
+            "<td>" +
+              "<span class='copy-machine' id='" + stock_number + "'>Copy</span>" +
+            "</td>" +
+          "</tr>" +
+          // hidden values
+          "<tr class='machine-preview-stats' id='machine_" + stock_number + "' hidden>" +
+            "<td id='" + stock_number + "_make' hidden>" + value['machine_make'] + "</td>" +
+            "<td id='" + stock_number + "_model' hidden>" + value['machine_model'] + "</td>" +
+            "<td id='" + stock_number + "_type' hidden>" + value['machine_type'] + "</td>" +
+            "<td id='" + stock_number + "_year' hidden>" + value['year'] + "</td>" +
+            "<td id='" + stock_number + "_serial' hidden>" + value['serial'] + "</td>" +
+            "<td id='" + stock_number + "_hours' hidden>" + value['hours'] + "</td>" +
+            "<td id='" + stock_number + "_price' hidden>" + value['price'] + "</td>" +
+            "<td id='" + stock_number + "_address' hidden>" + JSONaddress + "</td>" +
+            "<td id='" + stock_number + "_city' hidden>" + JSONcity + "</td>" +
+            "<td id='" + stock_number + "_state' hidden>" + JSONstate + "</td>" +
+            "<td id='" + stock_number + "_zip' hidden>" + JSONzip + "</td>" +
+            "<td id='" + stock_number + "_phone' hidden>" + JSONphone + "</td>" +
+            "<td id='" + stock_number + "_eaAccount' hidden>" + JSONeaAccount + "</td>" +
+            "<td id='" + stock_number + "_description' hidden>" + value['description'] + "</td>" +
+          "</tr>"
+        );
+      });
+    });
+  });
+  sidebarLoaded = true;
+  sidebarOpen = false;
+}
+
+// Click button to toggle sidebar and remove button
 $(document).on('click', '#showInventoryButton', function() {
   if (buttonDisplayed) {
     var inventoryButton = document.getElementById('showInventoryButton');
@@ -64,103 +148,42 @@ $(document).on('click', '#showInventoryButton', function() {
 });
 
 // Inject sidebar into page
-var sidebarOpen;
-var sidebarLoaded = false;
 function toggleSidebar() {
-  if (sidebarLoaded) {
-    $('#inventorySidebar').toggle('slide', {'direction': 'right'}, 500);
-    if (sidebarOpen) {
-      $('body').animate({'width': '100%'}, 500);
-      sidebarOpen = false;
-    }
-    else {
-      $('body').animate({'width': '75%'}, 500);
-      sidebarOpen = true;
-    }
-    // need to get content source attribute working to refresh iframes in MT
-    $('#contentIFrame').attr('contentsrc', function (i, val) {
-      return val;
-    });
+  $('#inventorySidebar').toggle('slide', {'direction': 'right'}, 500);
+  if (sidebarOpen) {
+    $('body').animate({'width': '100%'}, 500);
+    sidebarOpen = false;
   }
   else {
-    var sidebar = document.createElement('div');
-    sidebar.id = "inventorySidebar";
-    $(sidebar).html("\
-      <div class='header'>\
-        <h2 class='streamline-logo'>Streamline <span id='close-popup'>x</span></h2>\
-      </div>\
-      <hr class='divider' />\
-      <h3 class='my-inventory-header'>My Inventory</h3>\
-      <div class='inventory-table-container'>\
-        <table class='inventory-table'>\
-          <tbody id='inventory'>\
-          </tbody>\
-        </table>\
-      </div>\
-    ");
-    $('body').append(sidebar);
-    $(sidebar).toggle('slide', {'direction': 'right'}, 500);
     $('body').animate({'width': '75%'}, 500);
-
-    // Loads inventory into sidebar by parsing json from heroku site
-    var streamlineAPI = 'https://machine-demo-app.herokuapp.com/machines.json';
-    $.getJSON(streamlineAPI, function(data) {
-      $(data).each(function(index, value) {
-        var locationAPI = 'https://machine-demo-app.herokuapp.com/locations/' + value['location'] + '.json';
-        $.getJSON(locationAPI, function(json) {
-          var JSONaddress = json.address;
-          var JSONcity = json.city;
-          var JSONstate = json.state;
-          var JSONzip = json.zip;
-          var JSONphone = json.phone;
-          var stock_number = value['stock_number']
-          $('#inventory').append(
-            "<tr class='machine-preview'>" +
-              "<td class='machine-preview-cell' colspan='3' id='" + stock_number + "'>" +
-                "<strong class='stock-number' id='" + stock_number + "'>" + stock_number + "</strong>" + " " +
-                "<small id='" + stock_number + "'>" +
-                  value['machine_make'] + " " + value['machine_model'] + " " + value['machine_type'] +
-                "</small>" + " " +
-              "</td>" +
-              "<td>" +
-                "<span class='copy-machine' id='" + stock_number + "'>Copy</span>" +
-              "</td>" +
-            "</tr>" +
-            // hidden values
-            "<tr class='machine-preview-stats' id='machine_" + stock_number + "' hidden>" +
-              "<td id='" + stock_number + "_make' hidden>" + value['machine_make'] + "</td>" +
-              "<td id='" + stock_number + "_model' hidden>" + value['machine_model'] + "</td>" +
-              "<td id='" + stock_number + "_type' hidden>" + value['machine_type'] + "</td>" +
-              "<td id='" + stock_number + "_year' hidden>" + value['year'] + "</td>" +
-              "<td id='" + stock_number + "_serial' hidden>" + value['serial'] + "</td>" +
-              "<td id='" + stock_number + "_hours' hidden>" + value['hours'] + "</td>" +
-              "<td id='" + stock_number + "_price' hidden>" + value['price'] + "</td>" +
-              "<td id='" + stock_number + "_address' hidden>" + JSONaddress + "</td>" +
-              "<td id='" + stock_number + "_city' hidden>" + JSONcity + "</td>" +
-              "<td id='" + stock_number + "_state' hidden>" + JSONstate + "</td>" +
-              "<td id='" + stock_number + "_zip' hidden>" + JSONzip + "</td>" +
-              "<td id='" + stock_number + "_phone' hidden>" + JSONphone + "</td>" +
-              "<td id='" + stock_number + "_description' hidden>" + value['description'] + "</td>" +
-            "</tr>"
-          );
-        });
-      });
-    });
     sidebarOpen = true;
-    sidebarLoaded = true;
-    // not working: need to get content source attribute working to refresh iframes in MT
-    $('#contentIFrame').attr('contentsrc', function (i, val) {
-      return val;
-    });
   }
+  // need to get content source attribute working to refresh iframes in MT
+  // $('#contentIFrame').attr('contentsrc', function (i, val) {
+  //   return val;
+  // });
 }
 
 // Click 'x' to close sidebar
 $(document).on('click', '#close-popup', function() {
   toggleSidebar();
-  displayButton();
 });
 
+function stockNumberCheck() {
+  var stockNumberArray = $('.stock-number').val();
+  console.log(stockNumberArray);
+
+  // if (elsREGEX.test(siteURL)) {
+  // }
+
+  // if (eaREGEX.test(siteURL)) {
+  // }
+
+  // if (mtREGEX.test(siteURL)) {
+  // }
+}
+
+// Auto fills when table row is clicked in sidebar
 $(document).on('click', '.machine-preview-cell', function(btn) {
   var stock_number    = btn.target.id;
   var year            = $('#' + stock_number + '_year').html();
@@ -170,26 +193,20 @@ $(document).on('click', '.machine-preview-cell', function(btn) {
   var serial          = $('#' + stock_number + '_serial').html();
   var hours           = $('#' + stock_number + '_hours').html();
   var price           = $('#' + stock_number + '_price').html();
-  var address         = $('#' + stock_number + '_address').html();
   var city            = $('#' + stock_number + '_city').html();
   var state           = $('#' + stock_number + '_state').html();
-  var zip             = $('#' + stock_number + '_zip').html();
   var phone           = $('#' + stock_number + '_phone').html();
+  var eaAccount       = $('#' + stock_number + '_eaAccount').html();
   var description     = $('#' + stock_number + '_description').html();
-
-  var elsREGEX = (/\w+:\/\/\w+.equipmentlocator.\w+\/*\w*/i);
-  var eaREGEX = (/\w+:\/\/\w+.equipmentalley.\w+\/*\w*/i);
-  var mtREGEX = (/\w+:\/\/dscrm.sandhills.\w+\/*\S*/i);
-  var siteURL = document.URL;
 
   // Equipment Locator autofill
   if (elsREGEX.test(siteURL)) {
     if ($('#EQUIPMENT_stock').val() !== stock_number) {
       confirm("Are you sure you want to copy" + " " +
-            stock_number + " " +
-            "into stock number" + " " +
-            $('#EQUIPMENT_stock').val() + "?"
-           );
+              stock_number + " " +
+              "into stock number" + " " +
+              $('#EQUIPMENT_stock').val() + "?"
+      );
     }
     else if ($('#EQUIPMENT_stock').val() === stock_number) {
       if ($('#EQUIPMENT_control').val() !== phone) {
@@ -237,11 +254,11 @@ $(document).on('click', '.machine-preview-cell', function(btn) {
   // Equipment Alley autofill
   if (eaREGEX.test(siteURL)) {
     if ($('#STOCK').val() !== stock_number) {
-      alert("I can't copy stock number" + " " +
-            stock_number + " " +
-            "into stock number" + " " +
-            $('#STOCK').val()
-           );
+      confirm("Are you sure you want to copy" + " " +
+              stock_number + " " +
+              "into stock number" + " " +
+              $('#EQUIPMENT_stock').val() + "?"
+      );
     }
     else if ($('#STOCK').val() === stock_number) {
       if ($('#Se_CategoryID').val() === "") {
@@ -252,45 +269,36 @@ $(document).on('click', '.machine-preview-cell', function(btn) {
         alert("You haven't chosen a manufacturer. Please select something first.");
       }
 
-      if ($('#MODEL').val() !== model) {
-        $('#MODEL').val(model).effect('highlight', 'slow');
+      // if ($('#MODEL').val() !== model) {
+      //   $('#MODEL').val(model).effect('highlight', 'slow');
+      // }
+
+      // if ($('#YEAR').val() !== year) {
+      //   $('#YEAR').val(year).effect('highlight', 'slow');
+      // }
+
+      // if ($('#SERIAL').val() !== serial) {
+      //   $('#SERIAL').val(serial).effect('highlight', 'slow');
+      // }
+
+      // if ($('#HOURS').val() !== hours) {
+      //   $('#HOURS').val(hours).effect('highlight', 'slow');
+      // }
+
+      if ($('#_Location').val() !== eaAccount) {
+        $('#_Location').val(eaAccount).effect('highlight', 'slow');
+        var locationSelect = $(document).find('#_Location');
+        locationSelect.change();
       }
 
-      if ($('#YEAR').val() !== year) {
-        $('#YEAR').val(year).effect('highlight', 'slow');
-      }
+      // if ($('#_RETAIL_PRICE').val() !== price) {
+      //   $('#_RETAIL_PRICE').val(price).effect('highlight', 'slow');
+      //   $('#_RETAIL_PRICE').change();
+      // }
 
-      if ($('#SERIAL').val() !== serial) {
-        $('#SERIAL').val(serial).effect('highlight', 'slow');
-      }
-
-      if ($('#HOURS').val() !== hours) {
-        $('#HOURS').val(hours).effect('highlight', 'slow');
-      }
-
-      if ($('#_PUBL_DLR_ADDRESS').val() !== address) {
-        $('#_PUBL_DLR_ADDRESS').val(address).effect('highlight', 'slow');
-      }
-
-      if ($('#_PUBL_DLR_CITY').val() !== city) {
-        $('#_PUBL_DLR_CITY').val(city).effect('highlight', 'slow');
-      }
-
-      if ($('#_STATE').val() !== state) {
-        $('#_STATE').val(state).effect('highlight', 'slow');
-      }
-
-      if ($('#_PUBL_DLR_ZIP').val() !== zip) {
-        $('#_PUBL_DLR_ZIP').val(zip).effect('highlight', 'slow');
-      }
-
-      if ($('#_RETAIL_PRICE').val() !== price) {
-        $('#_RETAIL_PRICE').val(price).effect('highlight', 'slow');
-      }
-
-      if ($('[name="DESCRIPTION"]').val() !== description) {
-        $('[name="DESCRIPTION"]').val(description).effect('highlight', 'slow');
-      }
+      // if ($('[name="DESCRIPTION"]').val() !== description) {
+      //   $('[name="DESCRIPTION"]').val(description).effect('highlight', 'slow');
+      // }
     }
   }
 
@@ -312,11 +320,11 @@ $(document).on('click', '.machine-preview-cell', function(btn) {
     // need to figure out price and location (table not currently displayed)
 
     if (mtStockNumber.val() !== stock_number) {
-      alert("I can't copy stock number" + " " +
-            stock_number + " " +
-            "into stock number" + " " +
-            mtStockNumber.val()
-           );
+      confirm("Are you sure you want to copy" + " " +
+              stock_number + " " +
+              "into stock number" + " " +
+              $('#EQUIPMENT_stock').val() + "?"
+      );
     }
     else if (mtStockNumber.val() === stock_number) {
       // Fills forms
