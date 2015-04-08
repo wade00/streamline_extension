15290855cfbd4eb2a523fb3de2486e8dc1529251
Wade Novak <wade.novak@gmail.com>
2015-04-08 16:35:28 -0500

    Removes Unecessary Functions

--- background.js
+++ background.js
@@ -4,16 +4,3 @@ chrome.runtime.onMessage.addListener(function(msg, sender) {
     chrome.pageAction.show(sender.tab.id);
   }
 });
-
-// Send request to current tab when page action is clicked
-chrome.pageAction.onClicked.addListener(function(tab) {
-  chrome.tabs.query({
-    active: true,
-    currentWindow: true
-  }, function(tabs) {
-    chrome.tabs.sendMessage(
-      tab.id,
-      {callFunction: 'toggleSidebar'}
-    );
-  });
-});
